f0b1607e2e90c2fa7a83f175ae115d424a576d5f
byrondal <miguel.fergon@yahoo.es>
2023-04-14 11:19:38 +0200

    add first file variables

---
+++ FUNDAMENTALS/01_Variables.js
@@ -0,0 +1,25 @@
+var text = 'Hola, qué tal?'; //cuidado cuando escribimos en inglés: "hi don't do it"
+var pmba = 'pmba';
+var finalText = text + ' ' + pmba;
+
+//FINAL_TEXT(PASCAL_CASE) = se llama Pascal Case, se usa para valores que no van a cambiar (estáticos) se usará en React.
+//finalText(CAMEL_CASE) = es la forma principal que vamos a utilizar para nombrar variables.
+
+
+console.log(finalText);
+
+//var greetings = 'hola, qué tal?
+//var school = "pmba"
+//var greetinsFromSchool = greetings + ' ' + school
+//utilizar lo máximo posible nombres descriptivos para las variables
+
+//string = texto
+//number = números  var age = 30 var ageAsString = '30'  var ageAsNumber = 30
+//boolean = True or False, solo puede tener dos valores (True o False)
+//array = conjunto de valores, var arrayOfValues = [30, '30', false, true, 4]
+//object = librerías que acumulan conjunto de valores, var objectOfValues = {age: 30, ageAsNumber: ageAsNumber}
+
+//A LOS PUNTO Y COMA LOS LLAMAMOS EN INGLÉS: "SEMICOLON"
+
+
+
