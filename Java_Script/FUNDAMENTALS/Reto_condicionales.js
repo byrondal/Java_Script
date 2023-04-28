a080b71d2fddc1a04f8a047335016ddc994f4456
byrondal <miguel.fergon@yahoo.es>
2023-04-28 18:12:33 +0200

    add new exercises and conditionals challenge

---
+++ Java_Script/FUNDAMENTALS/Reto_condicionales.js
@@ -0,0 +1,26 @@
+/*Crea una variable llamada "character" y asígnale el valor "Legolas"*/
+
+let character = 'Legolas';
+
+/*crea una variable llamada "race" y asígnale el valro "Elfo"*/
+
+let race = 'Elfo';
+
+/*Crea una variable llamada "hasRIng y asígnale el valor "false" (Legolas no tiene el Anillo Único)*/
+
+let hasRing = false;
+
+/* Crea una variable llamada "isArcher" y asígnale el valor "true" (Legolas es un arquero)*/
+
+let isArcher = true;
+
+/* Utiliza un condicional "if" para evaluar si "hasRing" es true. Si lo es, imprime en la consola el mensaje "Legolas es el portador del Anillo Único"*/
+
+if(hasRing === true){
+    console.log('Legolas es el portador del Anillo Único')
+} else if(isArcher === true){
+    console.log('Legolas es un arquero experto')
+} else {
+    console.log('Legolas es un guerrero valiente')
+}
+
